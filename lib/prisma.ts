da77fe011c689b704250f341869134d2c9fd1933
Volodymyr Kopytin <4933561+vkopytin@users.noreply.github.com>
2025-12-03 20:31:19 +0100

    updating conenect env var name

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from './generated/prisma/client'
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = `${process.env.STORE_PRISMA_DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
